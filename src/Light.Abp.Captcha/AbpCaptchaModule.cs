543387b1abfac60118b6619d769e18f637a80b5a
seventh88 <5856868x@gmail.com>
2021-04-25 16:02:10 +0800

    fix

--- src/Light.Abp.Captcha/AbpCaptchaModule.cs
+++ src/Light.Abp.Captcha/AbpCaptchaModule.cs
@@ -2,16 +2,23 @@
 using Volo.Abp.Localization;
 using Volo.Abp.Modularity;
 using Volo.Abp.Settings;
+using Volo.Abp.VirtualFileSystem;
 
 namespace Light.Abp.Captcha
 {
     [DependsOn(
         typeof(AbpSettingsModule),
+        typeof(AbpVirtualFileSystemModule),
         typeof(AbpCachingModule))]
     public class AbpCaptchaModule : AbpModule
     {
         public override void ConfigureServices(ServiceConfigurationContext context)
         {
+            Configure<AbpVirtualFileSystemOptions>(options =>
+            {
+                options.FileSets.AddEmbedded<AbpCaptchaModule>();
+            });
+
             Configure<AbpLocalizationOptions>(options =>
             {
                 options.Resources
